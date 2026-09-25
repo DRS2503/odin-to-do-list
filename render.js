050381fa57e6850fdfb860e7c0ac4c1eec685938
DRS2503 <drschramm33@gmail.com>
2026-09-25 16:41:26 -0500

    Added render module and dialog creation
    Made function RenderNav to create DOM buttons on the right side
    based on the array of projects. Made renderMain to create the DOM
    of the tasks for that project selected. By default the app will
    render project at index 0.

---
+++ render.js
@@ -0,0 +1,84 @@
+export function renderMain(object){
+    //Set project header name
+    const projectName = document.querySelector('.project-header');
+    projectName.textContent = object.title;
+
+    //copies the task list from object
+    const taskList = object.getTaskList();
+
+    //main container that all the tasks will go into
+    const taskContainer = document.getElementById('task-container');
+    taskContainer.textContent = '';
+    
+    //makes each card div and fills content
+    for(const tasks in taskList){     
+        //Adding task card container to the task container
+        const taskCard = document.createElement('div');
+        taskCard.id = tasks;
+        taskCard.classList.add('task-card');
+        taskContainer.append(taskCard);
+
+        //Radio button and task header
+        const taskNameDiv = document.createElement('div');
+        const radioInput = document.createElement('input');
+        const taskNameHeader = document.createElement('h2');
+        taskNameDiv.classList.add('flex');
+        radioInput.type = 'radio';
+        taskNameHeader.textContent = taskList[tasks].title;
+        taskCard.append(taskNameDiv);
+        taskNameDiv.append(radioInput, taskNameHeader);
+
+        //Date
+        const taskContentContainer = document.createElement('div');
+        const dateLabel = document.createElement('p');
+        const dateData = document.createElement('p');
+        taskContentContainer.classList.add('task-content-container');
+        dateLabel.classList.add('label-text');
+        dateData.classList.add('data-text');
+        dateLabel.textContent = 'Target';
+        dateData.textContent = taskList[tasks].date;
+        taskCard.append(taskContentContainer);
+        taskContentContainer.append(dateLabel, dateData);
+
+        //Priority
+        const taskContentContainer2 = document.createElement('div');
+        const priorityLabel = document.createElement('p');
+        const priorityData = document.createElement('p');
+        taskContentContainer2.classList.add('task-content-container');
+        priorityLabel.classList.add('label-text');
+        priorityData.classList.add('data-text');
+        priorityLabel.textContent = 'Priority';
+        priorityData.textContent = taskList[tasks].priority;
+        taskCard.append(taskContentContainer2);
+        taskContentContainer2.append(priorityLabel, priorityData);
+
+        //Drop down
+        const taskContentContainer3 = document.createElement('div');
+        const svgText = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>chevron-down</title><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>'
+        taskContentContainer3.classList.add('task-content-container');
+        taskCard.append(taskContentContainer3);
+        taskContentContainer3.innerHTML = svgText;
+
+        //event listener for drop down
+    
+    }
+
+}
+
+export function renderNav(objectList){
+    const unorderedList = document.querySelector('ul');
+
+    for(const project in objectList){
+        const projectListItem = document.createElement('li');
+        const projectButton = document.createElement('button');
+        projectButton.classList.add('nav-button');
+        projectButton.textContent = objectList[project].title;;
+        unorderedList.append(projectListItem);
+        projectListItem.append(projectButton);
+
+        projectButton.addEventListener('click', () => {
+            const target = objectList[project];
+            renderMain(target);
+        })
+    }
+}
--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+import { renderMain, renderNav } from './render.js';
+
 const projects = [];
 
 function project(title, date){
@@ -7,8 +9,8 @@ function project(title, date){
         return taskList
     }
 
-    const addTask = (object) => {
-        taskList.push(object)
+    const addTask = (task) => {
+        taskList.push(task)
         //Render Again
     }
 
@@ -24,59 +26,10 @@ function task(title, date, priority, description){
     return {title, date, priority, description};
 }
 
-function render(object){
-    const taskList = object.getTaskList();
-    const taskContainer = document.getElementById('task-container');
-    
-    for(const tasks in taskList){     
-        //Adding task card container to the task container
-        const taskCard = document.createElement('div');
-        taskCard.id = tasks;
-        taskCard.classList.add('task-card');
-        taskContainer.append(taskCard);
-
-        //Radio button and task header (Inner Left)
-        const taskNameDiv = document.createElement('div');
-        const radioInput = document.createElement('input');
-        const taskNameHeader = document.createElement('h2');
-        taskNameDiv.classList.add('flex');
-        radioInput.type = 'radio'
-        taskNameHeader.textContent = taskList[tasks].title;
-        taskCard.append(taskNameDiv);
-        taskNameDiv.append(radioInput, taskNameHeader);
-
-        //Date (inner center)
-        const taskContentContainer = document.createElement('div')
-        const dateLabel = document.createElement('p');
-        const dateData = document.createElement('p');
-        taskContentContainer.classList.add('task-content-container');
-        dateLabel.classList.add('label-text');
-        dateData.classList.add('data-text');
-        dateLabel.textContent = 'Target';
-        dateData.textContent = taskList[tasks].date
-        taskCard.append(taskContentContainer);
-        taskContentContainer.append(dateLabel, dateData);
-
-        const taskContentContainer2 = document.createElement('div')
-        const priorityLabel = document.createElement('p');
-        const priorityData = document.createElement('p');
-        taskContentContainer.classList.add('task-content-container2');
-        priorityLabel.classList.add('label-text');
-        priorityData.classList.add('data-text');
-        priorityLabel.textContent = 'Priority';
-        priorityData.textContent = taskList[tasks].priority;
-        taskCard.append(taskContentContainer2);
-        taskContentContainer2.append(priorityLabel, priorityData);
-
-
-    
-    }
-
-}
-
+const projectDialog = document.getElementById('project-dialog');
 const newProjectBtn = document.getElementById('new-project-btn');
 newProjectBtn.addEventListener('click', () => {
-    console.log('New Project Clicked');
+    projectDialog.showModal();
 })
 
 const newTaskBtn = document.getElementById('new-task-btn');
@@ -85,14 +38,26 @@ newTaskBtn.addEventListener('click', () => {
 })
 
 
+/* Place Holders */
 const project1 = project('Home Chores', 'none');
+const project2 = project('Work', 'none');
 
 const task1 = task('Dishes', 'today', 'high', 'take dishes out and reload dishwasher');
 const task2 = task('Mow The Lawn', 'tomorrow', 'Low', 'Cut the grass its getting long');
+const task3 = task('Dishes', 'today', 'high', 'take dishes out and reload dishwasher');
+const task4 = task('Mow The Lawn', 'tomorrow', 'Low', 'Cut the grass its getting long');
 
 project1.addTask(task1);
 project1.addTask(task2);
+project1.addTask(task3);
+project1.addTask(task4);
 
-render(project1);
+project2.addTask(task4);
+project2.addTask(task1);
+
+projects.push(project1, project2);
+
+renderNav(projects);
+renderMain(projects[0]);
 
 
